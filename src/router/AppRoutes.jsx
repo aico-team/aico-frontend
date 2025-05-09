88f693e8a0808e8ef0dcc7f9a2b4df5d17730641
ezzzni <unin5412@gmail.com>
2025-05-09 21:48:46 +0900

    feat: Layout 및 Sidebar 컴포넌트 기본 구조 생성

--- src/router/AppRoutes.jsx
+++ src/router/AppRoutes.jsx
@@ -5,6 +5,7 @@ import App from "../App";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Dashboard from "../pages/Dashboard";
+import Layout from "../components/layout/Layout";
 
 export default function AppRoutes() {
   return (
@@ -13,8 +14,10 @@ export default function AppRoutes() {
         <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        {/* 다른 라우트들 */}
+        <Route element={<Layout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          {/* 다른 라우트들 */}
+        </Route>
       </Routes>
     </Router>
   );
