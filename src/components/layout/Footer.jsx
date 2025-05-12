e930026bc5f49817dd9b8fb2b2186116c3e7eda8
ezzzni <unin5412@gmail.com>
2025-05-13 01:41:26 +0900

    feat: 랜딩페이지 기본 스타일 적용

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className="layout-footer">
       <p>@ 2025 Aico Team</p>
     </footer>
   );
--- src/components/layout/Landing.jsx
+++ src/components/layout/Landing.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+//import "../styles/Landing.css";
+import "../../styles/Layout.css";
 
 const Landing = () => {
   return (
