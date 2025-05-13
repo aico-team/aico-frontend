48599c2b0e4f22670e1593f0fc4f2d2ff0a1a8a6
ezzzni <unin5412@gmail.com>
2025-05-14 01:57:21 +0900

    chore: react-icons 적용 및 적용에 따른 스타일 정비

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../../styles/Sidebar.css";
+//아이콘
+import { AiFillHome, AiFillSchedule } from "react-icons/ai";
+import { MdAccessTimeFilled, MdGroups, MdLogout } from "react-icons/md";
+import { FcSettings } from "react-icons/fc";
+import { RiAiGenerate } from "react-icons/ri";
+import { IoMdList, IoIosChatboxes } from "react-icons/io";
+import { FaListCheck, FaBarsProgress } from "react-icons/fa6";
+import { IoLogoWechat } from "react-icons/io5";
 
 const Sidebar = () => {
   return (
@@ -8,45 +16,81 @@ const Sidebar = () => {
       <nav className="sidebar-menu">
         <ul>
           <li>
-            <Link to="/dashboard">메인 홈</Link>
+            <Link to="/dashboard">
+              <AiFillHome />
+              메인 홈
+            </Link>
           </li>
           <li>
-            <Link to="/">공부 시간</Link>
+            <Link to="/">
+              <MdAccessTimeFilled />
+              공부 시간
+            </Link>
           </li>
           <li>
-            <Link to="/TopicInput">커리큘럼 생성</Link>
+            <Link to="/TopicInput">
+              <RiAiGenerate />
+              커리큘럼 생성
+            </Link>
           </li>
           <li>
-            <Link to="/CurriculumList">커리큘럼 목록</Link>
+            <Link to="/CurriculumList">
+              <IoMdList />
+              커리큘럼 목록
+            </Link>
           </li>
           <li>
-            <Link to="/">챗봇과의 대화</Link>
+            <Link to="/">
+              <IoIosChatboxes />
+              챗봇과의 대화
+            </Link>
           </li>
         </ul>
 
         <div className="sidebar-subsection">상세 페이지</div>
         <ul>
           <li>
-            <Link to="/">월간 플래너</Link>
+            <Link to="/">
+              <AiFillSchedule />
+              월간 플래너
+            </Link>
           </li>
           <li>
-            <Link to="/">to-do 리스트</Link>
+            <Link to="/">
+              <FaListCheck />
+              to-do 리스트
+            </Link>
           </li>
           <li>
-            <Link to="/">그룹</Link>
+            <Link to="/">
+              <MdGroups />
+              그룹
+            </Link>
           </li>
           <li>
-            <Link to="/">나의 진행도</Link>
+            <Link to="/">
+              <FaBarsProgress />
+              나의 진행도
+            </Link>
           </li>
           <li>
-            <Link to="/">채팅</Link>
+            <Link to="/">
+              <IoLogoWechat />
+              채팅
+            </Link>
           </li>
         </ul>
       </nav>
 
       <div className="sidebar-section sidebar-bottom">
-        <Link to="/">설정</Link>
-        <Link to="/">로그아웃</Link>
+        <Link to="/">
+          <FcSettings />
+          설정
+        </Link>
+        <Link to="/">
+          <MdLogout />
+          로그아웃
+        </Link>
       </div>
     </aside>
   );
