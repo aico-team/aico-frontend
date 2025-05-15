997967cb1493e5ad15717c60a73dbfbd0a18df2c
ezzzni <unin5412@gmail.com>
2025-05-16 00:29:07 +0900

    chore: App 컴포넌트를 AppRoutes로 위임하는 구조로 변경

--- src/router/AppRoutes.jsx
+++ src/router/AppRoutes.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import App from "../App";
 
 import Login from "../pages/Login";
 import Register from "../pages/Register";
