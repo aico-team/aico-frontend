c8e6212e2f55bde1fbf2357c640d369ae77c1e36
ezzzni <unin5412@gmail.com>
2025-05-18 15:03:45 +0900

    feat: 챗봇 질의응답 페이지 구조 설정

---
+++ src/pages/QnaPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const QnaPage = () => {
+  return (
+    <div>
+      <div className="qna-wrapper">
+        <h1 className="qna-title">💬 챗봇과의 대화</h1>
+      </div>
+    </div>
+  );
+};
+
+export default QnaPage;
