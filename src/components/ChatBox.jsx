905c51a9c5a763e7dbed028cf2f86336bb3653ac
ezzzni <unin5412@gmail.com>
2025-05-18 15:21:14 +0900

    feat: ChatBox 컴포넌트 기본 구조 구성 및 QnaPage에 렌더링

---
+++ src/components/ChatBox.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ChatBox = () => {
+  return (
+    <div>
+      <div>
+        <p>Chatbox</p>
+      </div>
+    </div>
+  );
+};
+
+export default ChatBox;
--- src/pages/QnaPage.jsx
+++ src/pages/QnaPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import ChatBox from "../components/ChatBox";
 
 const QnaPage = () => {
   return (
     <div>
       <div className="qna-wrapper">
         <h1 className="qna-title">💬 챗봇과의 대화</h1>
+        <ChatBox />
       </div>
     </div>
   );
