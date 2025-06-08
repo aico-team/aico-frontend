f450dfadcf402d361b401b7ea1c4f75bf23ee530
ezzzni <unin5412@gmail.com>
2025-06-08 23:23:45 +0900

    feat: 오답 노트 퀴즈 카드 대시보드에 렌더링 구현

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,20 +1,29 @@
 import React, { useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
 import StudyTimer from "../components/common/StudyTimer";
 import useAuthStore from "../../stores/authStore";
 import useStudyStatStore from "../../stores/studyStatStore";
+import useWrongNoteStore from "../../stores/wrongNoteStore";
 import CalendarMiniCard from "../components/dashboard/CalendarMiniCard";
+import DashboardQuizCard from "../components/dashboard/DashboardQuizCard";
 import CurriculumProgressCard from "../components/dashboard/CurriculumProgressCard";
 import "../styles/Dashboard.css";
+import "swiper/css";
+import "swiper/css/pagination";
 
 const Dashboard = () => {
   const { user } = useAuthStore();
   const { streakCount, fetchStreakCount } = useStudyStatStore();
+  const { wrongQuizzes } = useWrongNoteStore();
 
   useEffect(() => {
     if (user?.userId) {
       fetchStreakCount(user.userId);
     }
   }, [user?.userId]);
+
+  const flattenedQuizzes = wrongQuizzes.flatMap((file) => file.quizzes);
 
   return (
     <div>
@@ -35,11 +44,29 @@ const Dashboard = () => {
         </div>
       </div>
 
-      <div className="card-grid">
-        <div className="full-card message-card">
-          <p>메시지 기능 준비중...</p>
+      <div className="quiz-and-curriculum-wrapper">
+        <div className="quiz-column">
+          {flattenedQuizzes.length > 0 && (
+            <Swiper
+              modules={[Pagination]}
+              pagination={{ clickable: true }}
+              spaceBetween={20}
+              slidesPerView={1}
+            >
+              {flattenedQuizzes.map((item, idx) => (
+                <SwiperSlide key={item.id}>
+                  <DashboardQuizCard
+                    index={idx + 1}
+                    quiz={item.quiz}
+                    answer={item.answer}
+                  />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          )}
         </div>
-        <div className="full-card">
+
+        <div className="curriculum-column">
           <CurriculumProgressCard />
         </div>
       </div>
